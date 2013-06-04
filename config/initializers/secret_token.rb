f76b2e39ca35edeaa7b0f9efe6a9e622e8816c92
santila <santila.kvn@gmail.com>
2013-06-04 18:34:40 +0200

    Initialimport

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fileupload::Application.config.secret_token = 'db4873f51eee592748c40ee35be660383d7c77a8531792d7124c96800ab72ebde9af224bc65c0facedc3e283d9688b2e8a8d8c4aeade970f3b55e35a95f2ded9'
